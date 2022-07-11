2d8abb0fd7cc6f05a83ef44dc276a37b0ac5e0a7
김지성 <77275224+adk2421@users.noreply.github.com>
2022-07-11 12:10:32 +0900

    member Service 작성

--- src/main/java/idmb/admin/member/AdminMemberService.java
+++ src/main/java/idmb/admin/member/AdminMemberService.java
@@ -1,7 +1,15 @@
 package idmb.admin.member;
 
+import java.util.Map;
+
+import idmb.model.MemberBean;
+
 public interface AdminMemberService {
 	
-	
+	public Map<String, Object> selectMember(MemberBean member) throws Exception;
+
+    public Map<String, Object> updateMember(MemberBean member) throws Exception;
+
+    public void dropMember(MemberBean member) throws Exception;
 	
 }
