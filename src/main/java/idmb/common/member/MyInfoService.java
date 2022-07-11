a39ed6cd0ccb9712b8a3937a8e96227502541ec2
김지성 <77275224+adk2421@users.noreply.github.com>
2022-07-11 14:20:48 +0900

    memberService 작성

--- src/main/java/idmb/common/member/MyInfoService.java
+++ src/main/java/idmb/common/member/MyInfoService.java
@@ -1,5 +1,14 @@
 package idmb.common.member;
 
-public class MyInfoService {
+import java.util.Map;
 
+import idmb.model.MemberBean;
+
+public interface MyInfoService {
+
+    public Map<String, Object> checkPw(MemberBean member) throws Exception;
+
+    public Map<String, Object> updateMember(MemberBean member) throws Exception;
+
+    public void dropMember(MemberBean member) throws Exception;
 }
