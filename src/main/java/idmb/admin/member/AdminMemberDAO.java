d62fca84950d6944fa00320a48777d005d0c899f
김지성 <77275224+adk2421@users.noreply.github.com>
2022-07-11 13:42:58 +0900

    memberDAO 작성

--- src/main/java/idmb/admin/member/AdminMemberDAO.java
+++ src/main/java/idmb/admin/member/AdminMemberDAO.java
@@ -1,5 +1,31 @@
 package idmb.admin.member;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository("adminMemberDAO")
 public class AdminMemberDAO {
 
+    @Resource(name="sqlSessionTemplate")
+	private SqlSessionTemplate sqlSessionTemplate;
+
+    // 아이디 확인
+	public Map<String, Object> confirmId(Map<String, Object> map) throws Exception {
+		return sqlSessionTemplate.selectOne("member.confirmId", map);
+	}
+
+    // 회원 정보 변경
+	public void updateMember(Map<String, Object> map) throws Exception{
+		sqlSessionTemplate.update("member.updateMember",map);
+	}
+	
+	// 회원 삭제
+	public void dropMember(Map<String, Object> map) throws Exception{
+		sqlSessionTemplate.update("member.dropMember",map);
+	}
 }
