1de5a7d96b0add403b28eddd5b05324e3c517906
김지성 <77275224+adk2421@users.noreply.github.com>
2022-07-19 10:11:56 +0900

    addressAPI.js 수정

--- src/main/webapp/resources/js/addressAPI.js
+++ src/main/webapp/resources/js/addressAPI.js
@@ -1,6 +1,5 @@
 // addressAPI.js	
 	
-<<<<<<< HEAD
 	function execDaumPostcode() {
 	    new daum.Postcode({
 	        oncomplete: function(data) {
@@ -12,16 +11,3 @@
 	        }
 	    }).open();
     }
-=======
-function execDaumPostcode() {
-	new daum.Postcode({
-		oncomplete: function(data) {
-			
-		document.getElementById('postcode').value = data.zonecode;
-		document.getElementById('address1').value = data.roadAddress;
-		document.getElementById('address').value = "[" + data.zonecode + "] " + data.roadAddress;
-	
-		}
-	}).open();
-}
->>>>>>> branch 'main' of https://github.com/adk2421/IDMB
