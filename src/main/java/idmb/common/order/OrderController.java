d317a020c82a38c74652b80a35bfb3fee6abaf16
bk1828 <cbk1828@gmail.com>
2022-07-22 13:41:56 +0900

    basket 작성

--- src/main/java/idmb/common/order/OrderController.java
+++ src/main/java/idmb/common/order/OrderController.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import idmb.common.member.MyInfoService;
 import idmb.common.product.ProductService;
